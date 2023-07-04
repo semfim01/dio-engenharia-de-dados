6ec6f7582cc3a0578cde0fd355ece26ba7e398fc
semfim01 <wobarret@gmail.com>
2023-07-04 11:30:30 -0300

    ok
    com ajuda do chatgpt conseguir resolver o erro do caminho

--- seg_infor_com_python/comparador_de_hashes/ch.py
+++ seg_infor_com_python/comparador_de_hashes/ch.py
@@ -1,7 +1,8 @@
 import hashlib
+import os
 
-arquivo1 = 'a.txt'
-arquivo2 = 'a.txt'
+arquivo1 = os.path.join(os.path.dirname(__file__), 'a.txt')
+arquivo2 = os.path.join(os.path.dirname(__file__), 'b.txt')
 
 hash1 = hashlib.new('ripemd160')
 hash1.update(open(arquivo1,'rb').read())
@@ -12,6 +13,6 @@ hash2.update(open(arquivo2,'rb').read())
 
 if hash1.digest() != hash2.digest(): 
 #resume as informções
-    print(f' O arquivo: {arquivo1} é diferente do arquivo: {arquivo2}')
+    print(f' O arquivo: "{arquivo1}" é diferente do arquivo: "{arquivo2}"')
 else:
-    print(f'O arquivo: {arquivo1} é igual ao arquivo: {arquivo2}')
+    print(f'O arquivo: "{arquivo1}" é igual ao arquivo: "{arquivo2}"')
